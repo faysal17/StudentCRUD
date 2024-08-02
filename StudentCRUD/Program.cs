a1df4877246ade2606b1716f8e291403fc193634
Faysal Mahmud <82459799+faysal17@users.noreply.github.com>
2024-08-02 14:53:27 +0600

    Create the database

--- StudentCRUD/Program.cs
+++ StudentCRUD/Program.cs
@@ -25,6 +25,13 @@ else
     app.UseMigrationsEndPoint();
 }
 
+using (var scope  = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<SchoolContext>();
+    context.Database.EnsureCreated();
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
