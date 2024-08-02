1fc42ce729dabdee78e19653cf8fdb309946647b
Faysal Mahmud <82459799+faysal17@users.noreply.github.com>
2024-08-02 12:00:38 +0600

    Add course class Created course model class

---
+++ StudentCRUD/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentCRUD.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
