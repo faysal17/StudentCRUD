b5a3aa4e127a257b16d78bda35b06fd746a39529
Faysal Mahmud <82459799+faysal17@users.noreply.github.com>
2024-08-02 12:23:41 +0600

    Scaffold student pages

---
+++ StudentCRUD/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentCRUD.Data;
+using StudentCRUD.Models;
+
+namespace StudentCRUD.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly StudentCRUD.Data.SchoolContext _context;
+
+        public IndexModel(StudentCRUD.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Student != null)
+            {
+                Student = await _context.Student.ToListAsync();
+            }
+        }
+    }
+}
